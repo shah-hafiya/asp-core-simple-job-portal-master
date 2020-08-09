57469bd3358ad6c2b0cab54ad239f492bb722363
Hafiya <shah.hafiya@gmail.com>
2020-08-09 20:25:16 +0100

    Add project files.

---
+++ JobsApi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobPortal
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsApi : ControllerBase
+    {
+        // GET: api/<JobsApi>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<JobsApi>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<JobsApi>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<JobsApi>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JobsApi>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ServiceApiController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobPortal
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceApiController : ControllerBase
+    {
+        //https://localhost:5001/api/serviceapi
+        // GET: api/<ServiceApiController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<ServiceApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public string GetIpAddress()
+        {
+            string url = "http://edns.ip-api.com/json";
+            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.Method = "GET";
+            //request.UserAgent = RequestConstants.UserAgentValue;
+            //request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            var content = string.Empty;
+            using (var response = (System.Net.HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+
+            return content;
+        }
+
+        // POST api/<ServiceApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ServiceApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ServiceApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
